830ad455354bcc3073b2de1f01999eaebd906ac4
Bill <billfeng2002@gmail.com>
2021-06-09 13:11:32 -0700

    pets lab with Ekow

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,18 @@
 class Cat
-  # code goes here
+	@@all = []
+	@@count = 0
+	attr_reader :name
+	attr_accessor :owner, :mood
+	def initialize(name, owner, mood="nervous")
+		@name = name
+		@owner = owner
+		@mood = mood
+		@@all << self
+		@@count += 1
+	end
+	def self.all
+		@@all
+	end
+	
+	
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,65 @@
 class Owner
   # code goes here
+  attr_reader :name, :species
+  @@all=[]
+  def initialize(name)
+    @name=name
+    @species="human"
+    @@all << self
+  end
+  def say_species
+    "I am a #{self.species}."
+  end
+  def self.all
+    @@all
+  end
+
+  def self.count
+    self.all.count
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+
+  #associations
+  def cats
+    Cat.all.select{|cat| cat.owner == self}
+  end
+
+  def dogs
+    Dog.all.select{|dog| dog.owner == self}
+  end
+
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
+
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+
+  def walk_dogs
+    self.dogs.each{|dog| dog.mood="happy"}
+  end
+
+  def feed_cats
+    cats.each{|cat| cat.mood = "happy"}
+  end
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
+
+  def sell_pets
+    cats.each do |cat|
+      cat.mood = "nervous"
+      cat.owner = nil
+    end
+
+    dogs.each do |dog|
+      dog.mood = "nervous"
+      dog.owner = nil
+    end
+  end
 end
